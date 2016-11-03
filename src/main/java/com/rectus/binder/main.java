73b969b2e32aa236409e6400a99c74cafd6b97e6
a.bernard <a.bernard@e-deal.com>
2016-11-03 17:55:05 +0100

    main commit

--- src/main/java/com/rectus/binder/main.java
+++ src/main/java/com/rectus/binder/main.java
@@ -12,5 +12,22 @@ package com.rectus.binder;
 /*                  Class main                         */
 /*-----------------------------------------------------*/
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class main {
+
+	public static void main(String[] args) {
+
+		try {
+			Robot robot = new Robot();
+
+			// Simulate a key press
+			robot.keyPress(KeyEvent.VK_A);
+			robot.keyRelease(KeyEvent.VK_A);
+
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
 }
